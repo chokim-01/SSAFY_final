552020098d7bff59001cef5ad5dafe6b3aca1d1f
default <chokim159@naver.com>
2019-10-15 17:37:15 +0900

    S1P3151002-88 [+] Add SSL check background.js

--- Extension/background.js
+++ Extension/background.js
@@ -1,3 +1,4 @@
+/* Click -> open inject.js */
 chrome.browserAction.onClicked.addListener(
     function (tab) {
         console.log("Start section");
@@ -6,7 +7,43 @@ chrome.browserAction.onClicked.addListener(
     });
 });
 
+/* New tab, url changed */
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    console.log("tab url");
-    console.log(tab.url);
+    var url = tab.url;
+    var findstr = "https://"
+    if (url !== undefined && changeInfo.status == "complete") {
+      console.log("tab changed");
+
+      // Check https
+      if(url.indexOf(findstr) == -1) {
+          console.log("No https");
+      }
 });
+
+/* Communication with js */
+chrome.runtime.onConnect.addListener(function(port) {
+    console.log("Call onConnect");
+    port.onMessage.addListener(function(Message) {
+        console.log(Message);
+        console.log(port);
+    });
+});
+
+/* The Web Request API */
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+      if(details.method == "POST") {
+        console.log(details)
+        let pw = details.requestBody.formData
+        if(pw && pw.pw) {
+            if(pw.pw[0] == "test") {
+              alert("Un secured");
+              window.location.reload();
+              return { cancel: true };
+          }
+        }
+      }
+    },
+    {urls: ["<all_urls>"]},
+    ["blocking", "requestBody"]
+);
