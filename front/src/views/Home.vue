b2adae327834afa1460cc6935790e9ba981b5106
jin__y <jinneey05@gmail.com>
2019-10-16 10:51:24 +0900

    S1P3151002-56 [+] Init Implement MainPage

---
+++ front/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <v-container>
+      <v-row justify="center" align-content>
+        <v-btn>
+          <span>Add to Chrome</span>
+        </v-btn>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
