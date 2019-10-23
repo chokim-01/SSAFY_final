a3904b8d7ea50a096d0a4dabe69869b491110d0c
jin__y <jinneey05@gmail.com>
2019-10-23 14:06:10 +0900

    [+]Add User Edit part / Add  app.py mysql update

--- front/src/components/UserInfo.vue
+++ front/src/components/UserInfo.vue
@@ -6,12 +6,16 @@
           <v-icon>mdi-account</v-icon>
         </v-btn>
       </template>
+
       <v-card width="200">
         <v-flex xs8>
+          <!-- UserName -->
           <v-card-text>
             {{userInfo.name}}
             <br />
           </v-card-text>
+
+          <!-- User LogOut / Edit -->
           <v-card-text>
             <v-btn @click="signOut()" color="error">
               <v-icon>mdi-logout</v-icon>
@@ -20,6 +24,8 @@
               <v-icon>mdi-account-edit</v-icon>
             </v-btn>
           </v-card-text>
+
+          <!-- Admin -->
           <v-card-text>
             <v-list v-if="userInfo.grade=='master'" class="text-center">
               <v-btn to="/adminpage" color="warning">관리자페이지</v-btn>
---
+++ front/src/views/UserInfo.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <UserInfoUpdate />
+  </v-container>
+</template>
+
+<script>
+export default {
+    name : "UserInfo",
+    data: () => ({
+    }),
+    components:{
+        UserInfoUpdate: () => import("@/components/UserInfoUpdate")
+    }
+}
+</script>
