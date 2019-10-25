91548bb4da485974f376e177a523663adce3e9e4
default <chokim159@naver.com>
2019-10-25 09:10:45 +0900

    S1P3151002-108 [+] Edit background.js error fix

--- Previewer/background.js
+++ Previewer/background.js
@@ -1,33 +1,38 @@
 chrome.tabs.onUpdated.addListener((currentTabId, changeInfo, tab) => {
 	// Get User access url ex) https://naver.com
-	// Check http, https
-	urlStatus = checkURL(tab.url);
-	// If https
-	if(urlStatus === "https")
-	{
-		chrome.browserAction.setIcon({
-			path: {"38": "/Icons/38_secure.png"},
-			tabId: currentTabId
-		});
-	}
-	// If http
-	else if(urlStatus === "http")
-	{
-		chrome.browserAction.setIcon({
-			path: {"38": "/Icons/38_warning.png"},
-			tabId: currentTabId
-		});
-	}
-	// Unkown
-	else
-	{
-		console.log("Unknown");
-	}
+	url = tab.url;
 
-	// Get user input password
-	chrome.tabs.executeScript({
-        code:"document.addEventListener('keyup', function(){var elements = document.querySelectorAll('input[type=password]')[0]; if(elements) { var passwordName = elements.name; var passwordValue = elements.value; chrome.storage.local.set({ passwordInfo: [passwordName, passwordValue] }); } });"
+	if(url !== undefined && changeInfo.status ==="complete")
+	{
+		// Check http, https
+		urlStatus = checkURL(url);
+		// If https
+		if(urlStatus === "https")
+		{
+			chrome.browserAction.setIcon({
+				path: {"38": "/Icons/38_secure.png"},
+				tabId: currentTabId
+			});
+		}
+		// If http
+		else if(urlStatus === "http")
+		{
+			chrome.browserAction.setIcon({
+				path: {"38": "/Icons/38_warning.png"},
+				tabId: currentTabId
+			});
+		}
+		// Unkown
+		else
+		{
+			console.log("Unknown");
+		}
+
+		// Get user input password
+		chrome.tabs.executeScript({
+      code:"document.addEventListener('keyup', function(){var elements = document.querySelectorAll('input[type=password]')[0]; if(elements) { var passwordName = elements.name; var passwordValue = elements.value; chrome.storage.local.set({ passwordInfo: [passwordName, passwordValue] }); } });"
     });
+	}
 });
 
 chrome.webRequest.onBeforeRequest.addListener((requestData) => {
@@ -47,11 +52,11 @@ chrome.extension.onConnect.addListener((port) => {
           // get current tab info
           await chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
             var currTab = tabs[0];
-            if(currTab) { // Sanity check
+            if(currTab && checkURL(currTab.url) !== "unknown") { // Sanity check
               getsslData(currTab.url, port);
+		          secureCheck();
             }
           });
-          secureCheck();
         }
     });
 });
@@ -59,17 +64,20 @@ chrome.extension.onConnect.addListener((port) => {
 var checkURL = (url) => {
 	// Find "http" word in userAccessURL
 	let http_check = url.indexOf("http://");
-	if(http_check == 0)
-	{
-		return "http"
-	}
 	// Find "https" word in userAcessURL
 	let https_check = url.indexOf("https://");
-	if(https_check == 0)
-	{
-		return "https"
+	let result = "";
+
+	if(http_check == 0) {
+		result = "http";
 	}
-	return "unknown"
+	else if(https_check == 0) {
+		result = "https";
+	}
+	else {
+		result = "unknown";
+	}
+	return result;
 }
 
 var getsslData = async (url, port) => {
@@ -80,12 +88,11 @@ var getsslData = async (url, port) => {
     data: url,
     success: (data) => {
 			// send to inject.js
-			let httpStatus = checkURL(url)
-			port.postMessage([httpStatus, data]);
+			let urlStatus = checkURL(url)
+			port.postMessage([urlStatus, data]);
 			console.log(data);
     },
     error: (error) => {
-      console.log(error)
     }
   });
 }
@@ -105,7 +112,6 @@ var secureCheck = () => {
         console.log(data);
       },
       error: (error) => {
-        console.log(error)
       }
     });
   });
