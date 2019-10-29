cc50c062beee4eafef9477d74a2a1e9170087657
Kimyong <kkskww@gmail.com>
2019-10-29 11:15:08 +0900

    S1P3151002-111 [+] Add changeResult() function

--- front/src/components/TodayRequestList.vue
+++ front/src/components/TodayRequestList.vue
@@ -1,11 +1,20 @@
 <template>
   <v-container>
     <!-- Today Request List Data Table -->
-    <v-data-table :headers="headers" :items="todayRequestList" />
+    <v-data-table :headers="headers" :items="TodayRequest">
+      <template v-slot:item.analysisResult="{item}">
+        <!-- AnalysisResult Change -->
+        <v-chip
+          @click="changeResult(item.email,item.requestUrl)"
+        >{{analysisResult(item.analysisResult)}}</v-chip>
+      </template>
+    </v-data-table>
   </v-container>
 </template>
 
 <script>
+import Server from "../server.js"
+
 export default {
     props:{
       todayRequestList:{
@@ -27,6 +36,29 @@ export default {
                 value:"admission"
             }
         ]
-    })
+    }),
+    methods:{
+      changeResult(email,url){
+        let formData=new FormData()
+        formData.append("email",email)
+        formData.append("url",url)
+        Server(this.$store.state.SERVER_URL).post("/post/changeAnalysisResult", formData).then(result=>{
+          Server(this.$store.state.SERVER_URL).get("/get/todayRequest").then(result=>{
+            this.TodayRequest = []
+            for(var idx = 0; idx < result.data.length; idx++) {
+              this.TodayRequest.push({
+                email: result.data[idx].email,
+                requestUrl:result.data[idx].url,
+                analysisResult: result.data[idx].analysisResult
+              })
+            }
+          })
+        })
+      },
+      analysisResult(result){
+        if(result==1) return true
+        else return false
+      }
+    }
 }
 </script>
